c4e652ddbce92aa9039117e23b48e4e688044432
PW1_P4_TuLyt <nurainiaalima@gmail.com>
2024-03-21 01:07:36 +0700

    editing, routing, controlling/View

---
+++ Pertemuan 4/projetpw-root/app/Views/pages/about.php
@@ -0,0 +1,9 @@
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>halaman about</h1>
+                <p>Lorem ipsum dolor sit amet consectetur</p>
+            </div>
+        </div>
+    </div>
