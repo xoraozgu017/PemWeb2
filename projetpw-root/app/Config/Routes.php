b32b64aea39dea6d1c9ff359f72f90e56ed44c88
PW1_P4_TuLyt <nurainiaalima@gmail.com>
2024-03-27 16:43:32 +0700

    create layout in views

--- projetpw-root/app/Config/Routes.php
+++ projetpw-root/app/Config/Routes.php
@@ -5,8 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/about', 'Page::about');
-$routes->get('/contact', 'Page::contact');
-$routes->get('/faqs', 'Page::faqs');
+$routes->get('/', 'Pages::index');
+
 $routes->setAutoRoute(true);
