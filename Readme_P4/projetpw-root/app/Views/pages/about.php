f4cd5fd01dcbf7b6f9253c39c9c7563c8ec84357
xoraozgu017 <145304971+xoraozgu017@users.noreply.github.com>
2024-03-27 16:37:08 +0700

    Delete Readme_P4 directory

--- Readme_P4/projetpw-root/app/Views/pages/about.php
+++
@@ -1,9 +0,0 @@
-
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1>halaman about</h1>
-                <p>Lorem ipsum dolor sit amet consectetur</p>
-            </div>
-        </div>
-    </div>
